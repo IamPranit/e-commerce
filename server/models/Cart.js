f394ce4b724e38d7842f7b5e56480f8748a6c3a9
Pranit Maske <maskepranit@gmail.com>
2020-09-23 17:23:50 +0530

    - Added Carts Functionality
    - Needs to be implemented in the main project

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,26 +1,67 @@
 const mongoose = require("mongoose");
 
-const cartProductSchema = new mongoose.Schema({
-  productId: {
+const LineItemSchema = new mongoose.Schema({
+  lineItemProductId: {
     type: String,
     required: [true, "Please provide valid product ID"],
   },
-  productCounter: {
+  lineItemName: {
+    type: String,
+    required: [true, "Please provide product name"],
+  },
+  lineItemQuantity: {
     type: Number,
     min: 1,
-    required: [true, "Please provide needed number of product"],
+  },
+  lineItemPrice: {
+    type: Number,
   },
 });
 
 const CartSchema = new mongoose.Schema({
-  cartProducts: {
-    type: cartProductSchema,
-    required: [true, "Please add a product in the cart"],
-  },
-  cartUser: {
+  customerId: {
     type: String,
-    required: [true, "Please add the user's ID who's using this cart"],
   },
+  anonymousId: {
+    type: String,
+  },
+  customerEmail: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  cartItems: {
+    type: [LineItemSchema],
+  },
+  totalCartPrice: {
+    type: Number,
+    required: [true, "Total price of all the items in the cart is required"],
+  },
+  cartState: {
+    type: String,
+    enum: ["Active", "Merged", "Ordered"],
+    default: "Active",
+  },
+  shippingAddress: {
+    type: String,
+    required: [true, "Please add shipping address"],
+  },
+  paymentMethod: {
+    type: String,
+    enum: ["UPI", "COD", "Credit/Debit Card"],
+    default: "COD",
+  },
+});
+
+LineItemSchema.pre("save", function () {
+  const singleProductPrice = this.lineItemPrice;
+  this.lineItemPrice = singleProductPrice * this.lineItemQuantity;
+});
+
+CartSchema.pre("save", function () {
+  this.totalCartPrice = this.lineItemPrice;
 });
 
 module.exports = mongoose.model("Cart", CartSchema);
